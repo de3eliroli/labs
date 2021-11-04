4c88535bd51ee697e65750dba68996996b4fa3a1
Евгений Шеленков <91319891+de3eliroli@users.noreply.github.com>
2021-11-04 17:56:06 +0500

    Add files via upload

---
+++ Program1.pas
@@ -0,0 +1,73 @@
+﻿Var x,a,b,c,max:integer; 
+
+Begin 
+
+    Writeln('Введите трёхзначное число'); 
+
+    Readln(x); 
+
+    max:=0; 
+
+        a:=x div 100; 
+
+        b:=(x mod 100) div 10; 
+
+        c:=x mod 10; 
+
+    Writeln('Полученные числа:'); 
+
+        x := (a*100)+(b*10)+c; 
+
+      If x>max 
+
+        then max:=x; 
+
+        Writeln(x); 
+
+        x := (a*100)+(c*10)+b; 
+
+      If x>max 
+
+        then max:=x; 
+
+        Writeln(x); 
+
+        x := (b*100)+(a*10)+c; 
+
+      If x>max 
+
+        then max:=x; 
+
+        Writeln(x); 
+
+        x := (b*100)+(c*10)+a; 
+
+      If x>max 
+
+        then max:=x; 
+
+        Writeln(x); 
+
+        x := (c*100)+(a*10)+b; 
+
+      If x>max 
+
+        then max:=x; 
+
+        Writeln(x); 
+
+        x := (c*100)+(b*10)+a; 
+
+      If x>max 
+
+        then max:=x; 
+
+        Writeln((c*100)+(b*10)+a); 
+
+    Writeln('Максимальное число из полученных:'); 
+
+    Writeln(max) 
+
+ 
+
+end. 
---
+++ Program2.pas
@@ -0,0 +1,33 @@
+﻿Var a,b,n:integer; 
+
+Begin 
+
+    a:=random(1,99); 
+
+    n:=0; 
+
+    While (a<>b) and (n<>6)  
+
+    Do begin Write('Введите число: '); 
+
+             Readln(b); 
+
+             n:=n+1; 
+
+    If a<b  
+
+    then Writeln('меньше'); 
+
+    If a>b 
+
+    then Writeln('БОЛЬШЕ'); 
+
+       end;  
+
+If a=b  
+
+then Writeln('ЛУЧШИЙ!') 
+
+else Writeln('не лучший...') 
+
+End. 
